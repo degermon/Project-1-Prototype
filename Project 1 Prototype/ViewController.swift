9af8e9889b7d36a4db4af63c908d2de8f048c7a6
degermon <degermon@gmail.com>
2020-06-02 18:16:47 +0300

    Initial Commit

---
+++ Project 1 Prototype/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 1 Prototype
+//
+//  Created by Daniel Šuškevič on 2020-06-02.
+//  Copyright © 2020 Daniel Šuškevič. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
